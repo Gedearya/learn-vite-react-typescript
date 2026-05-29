ba961bcf38fe8ca9cb836ffc46fd41b7a0e22a8b
I Gede Arya Danny Pratama <igd.ad.pratama@gmail.com>
2026-05-29 14:38:22 +0700

    add dataTask

---
+++ src/data/index.ts
@@ -0,0 +1,24 @@
+import type { Task } from "../types";
+
+const dataTask: Task[] = [
+  {
+    id: 1,
+    title: "Breakfast",
+    description: "Bubur Ayam",
+    isComplete: true,
+  },
+  {
+    id: 2,
+    title: "Lunch",
+    description: "Bebek Goreng",
+    isComplete: false,
+  },
+  {
+    id: 1,
+    title: "Dinner",
+    description: "Soto Kudus",
+    isComplete: false,
+  },
+];
+
+export default dataTask;
